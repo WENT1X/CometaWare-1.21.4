1b71c2e5ef6a1c7637e3933393580ac2a4587df6
WENT1X <salievyt@gmail.com>
2025-07-20 20:45:31 +0600

    Update Hud , CordInformation

--- src/main/java/fun/exort/module/list/render/hud/Hud.java
+++ src/main/java/fun/exort/module/list/render/hud/Hud.java
@@ -14,9 +14,12 @@ import fun.exort.util.render.builders.states.SizeState;
 import fun.exort.util.render.msdf.MsdfFont;
 import fun.exort.util.render.renderers.impl.BuiltBlur;
 import fun.exort.util.render.renderers.impl.BuiltText;
+import net.minecraft.advancement.criterion.EntityHurtPlayerCriterion;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import org.joml.Matrix4f;
 
+import javax.swing.text.html.parser.Entity;
 import java.awt.*;
 import java.util.function.Supplier;
 
@@ -24,7 +27,7 @@ import java.util.function.Supplier;
 public class Hud extends Module {
 
     public BooleanSetting watermark = new BooleanSetting("Watermark", this, true);
-    public BooleanSetting coordinates = new BooleanSetting("CoordsInfo", this, false);
+    public BooleanSetting coordinates = new BooleanSetting("CoordsInfo", this, true);
     public BooleanSetting notification = new BooleanSetting("Notification", this, false);
 
     private static final Supplier<MsdfFont> BIKO_FONT = Suppliers.memoize(() -> MsdfFont.builder().atlas("biko").data("biko").build());
@@ -61,6 +64,7 @@ public class Hud extends Module {
                 .build();
         blur.render(matrix, 5, 5);
 
+
         BuiltText text = Builder.text()
                 .font(BIKO_FONT.get())
                 .size(10)
@@ -76,20 +80,20 @@ public class Hud extends Module {
 
         Matrix4f matrix = context.getMatrices().peek().getPositionMatrix();
 
-        float x = Math.round(mc.player.getPos().getX());
-        float y = Math.round(mc.player.getPos().getY());
-        float z = Math.round(mc.player.getPos().getZ());
+        int x = Math.toIntExact(Math.round(mc.player.getPos().getX()));
+        int y = Math.toIntExact(Math.round(mc.player.getPos().getY()));
+        int z = Math.toIntExact(Math.round(mc.player.getPos().getZ()));
 
         String coordsTitle = "X: " + x + ", Y: " + y + ", Z: " + z;
 
         BuiltBlur blur = Builder.blur()
                 .size(new SizeState(BIKO_FONT.get().getWidth(coordsTitle, 10) + 15, 20))
-                .color(new QuadColorState(new Color(30, 30, 30, 255)))
+                .color(new QuadColorState(new Color(74, 19, 163, 255)))
                 .radius(new QuadRadiusState(4))
                 .smoothness(1f)
                 .blurRadius(6)
                 .build();
-        blur.render(matrix, 5, 5 + 510);
+        blur.render(matrix, 5, 30);
 
         BuiltText text = Builder.text()
                 .font(BIKO_FONT.get())
@@ -98,6 +102,6 @@ public class Hud extends Module {
                 .thickness(0.02f)
                 .color(-1)
                 .build();
-        text.render(matrix, 11, 10.5f + 510);
+        text.render(matrix, 11, 35.5f);
     }
 }
