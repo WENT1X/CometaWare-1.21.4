904b7a362c604ad04131555bda77db1226f86ae9
WENT1X <salievyt@gmail.com>
2025-07-20 22:01:41 +0600

    Update HWID System

--- src/main/java/fun/exort/CometaWare.java
+++ src/main/java/fun/exort/CometaWare.java
@@ -5,9 +5,12 @@ import com.google.common.eventbus.Subscribe;
 import fun.exort.event.list.EventKeyInput;
 import fun.exort.module.Module;
 import fun.exort.module.ModuleStorage;
+import fun.exort.util.hwid.HWIDChecker;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.client.MinecraftClient;
+
+import static com.mojang.text2speech.Narrator.LOGGER;
 
 public class CometaWare implements ModInitializer {
 
@@ -26,6 +29,7 @@ public class CometaWare implements ModInitializer {
 		moduleStorage = new ModuleStorage();
 	}
 
+
 	public static CometaWare getInstance() {
 		return instance == null ? new CometaWare() : instance;
 	}
@@ -40,6 +44,10 @@ public class CometaWare implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		if (!HWIDChecker.isValidHWID()) {
+			LOGGER.error("Unauthorized HWID! Shutting down...");
+			throw new RuntimeException("Invalid HWID");
+		}
 		getModuleStorage().injectRegisterModules();
 	}
 
